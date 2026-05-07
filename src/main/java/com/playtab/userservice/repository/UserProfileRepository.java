6a580c0109cc22ef6bce3bf2d43251627eed4bc8
mikepark814-beep <mikepark814@pusan.ac.kr>
2026-05-07 17:20:55 +0900

    add admin user list and get RPCs

--- src/main/java/com/playtab/userservice/repository/UserProfileRepository.java
+++ src/main/java/com/playtab/userservice/repository/UserProfileRepository.java
@@ -1,6 +1,8 @@
 package com.playtab.userservice.repository;
 
 import com.playtab.userservice.entity.UserProfile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +11,5 @@ import java.util.UUID;
 public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
     Optional<UserProfile> findByIdentity_IdentityId(UUID identityId);
     Optional<UserProfile> findByEmail(String email);
+    Page<UserProfile> findByEmailContainingIgnoreCase(String emailFilter, Pageable pageable);
 }
